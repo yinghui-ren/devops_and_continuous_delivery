7044f7467aa08b0ce7b0f3a383e3a783334c32d9
Yinghui <yinghui.ren.cn@gmail.com>
2026-06-16 18:33:46 +0000

    Add sample web application for CI/CD pipeline

---
+++ app/test/utils/test_db.mjs
@@ -0,0 +1,27 @@
+// test/utils/test-db.mjs
+
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongoServer;
+
+async function connect() {
+  mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+}
+
+async function close() {
+  await mongoose.connection.dropDatabase();
+  await mongoose.connection.close();
+  await mongoServer.stop();
+}
+
+async function clearDatabase() {
+  const collections = mongoose.connection.collections;
+  for (const key in collections) {
+    await collections[key].deleteMany();
+  }
+}
+
+export default { connect, close, clearDatabase }; // Default export
